c12d34dc6a76898ea5c5c5b04cadb2445c4348f1
huseyin <huseyinsarsilmaz@hotmail.com>
2021-10-23 23:31:37 +0300

    Player and laser mechanichs changed

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+from operator import truediv
 import pygame
 from pygame import mixer
 from sys import exit
 import random
+import math
+
+from pygame import rect
 
 pygame.init()
 screen = pygame.display.set_mode((1200,700))
@@ -13,7 +17,7 @@ bomb = pygame.image.load("image/bomb.png").convert_alpha()
 beam = pygame.image.load("image/laser.png").convert_alpha()
 mixer.music.load("audio/music.mp3")
 laser_sound = mixer.Sound("audio/laser.mp3")
-mixer.music.play(loops = -1)
+#mixer.music.play(loops = -1)
 pygame.display.set_caption("Earth Defender")
 pygame.display.set_icon(spaceship)
 clock = pygame.time.Clock()
@@ -23,12 +27,22 @@ class Player(pygame.sprite.Sprite):
         super().__init__()
         self.image = spaceship
         self.rect = self.image.get_rect(center = (600,350))
+        self.angle = 0
+        self.dangle = 0
         self.dx = 0
         self.dy = 0
 
     def update(self):
+        self.image = pygame.transform.rotozoom(spaceship,-self.angle,1)
+        self.rect = self.image.get_rect(center=self.rect.center)
+        if(self.angle == -360 or self.angle == 360) : self.angle = 0
+        self.angle =  self.angle + self.dangle
         self.rect.x += self.dx
         self.rect.y += self.dy
+        if(self.rect.midleft[0] <= 0) : self.rect = self.image.get_rect(midleft = (0,self.rect.midleft[1]))
+        elif(self.rect.midright[0] >= 1200) : self.rect = self.image.get_rect(midright = (1200,self.rect.midright[1]))
+        if(self.rect.midtop[1] <= 0):  self.rect = self.image.get_rect(midtop = (self.rect.midtop[0],0))
+        elif(self.rect.midbottom[1] >= 700):  self.rect = self.image.get_rect(midbottom = (self.rect.midbottom[0],700))
 
 class Earth(pygame.sprite.Sprite):
     def __init__(self):
@@ -41,14 +55,18 @@ class Laser(pygame.sprite.Sprite):
         super().__init__()
         self.image = beam
         self.rect = self.image.get_rect()
+        self.angle = 0
         self.rect.x = -200
         self.rect.y = -200
         self.dy = -10
         self.shot = False
     
     def update(self): 
+        self.image = pygame.transform.rotozoom(beam,-self.angle,1)
+        self.rect = self.image.get_rect(center=self.rect.center)
         self.rect.y += self.dy
-        if(self.rect.y < 0) : self.shot = False
+        self.rect.x += self.dx
+        if(self.rect.y < 0 or self.rect.y > 700 or self.rect.x < 0 or self.rect.x > 1200) : self.shot = False
 
 class Enemy(pygame.sprite.Sprite):
     def __init__(self):
@@ -56,6 +74,7 @@ class Enemy(pygame.sprite.Sprite):
         self.image = ufo
         self.pos = [random.randint(0,1100),0]
         self.rect = self.image.get_rect(midbottom = self.pos)
+        self.dx = 0
         self.dy = 0.25
     def update(self): 
         self.pos[1] =self.pos[1] + self.dy
@@ -69,7 +88,9 @@ laser = pygame.sprite.GroupSingle()
 laser.add(Laser())
 enemy = pygame.sprite.Group()
 enemy_timer = pygame.USEREVENT + 1
-pygame.time.set_timer(enemy_timer,7500)
+pygame.time.set_timer(enemy_timer,3000)
+move = False
+reverse = False
 
 while True:
     for event in pygame.event.get(): 
@@ -77,21 +98,43 @@ while True:
             pygame.quit()
             exit()
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT: player.sprite.dx = -4
-            elif event.key == pygame.K_RIGHT: player.sprite.dx = 4
-            elif event.key == pygame.K_DOWN: player.sprite.dy = 4
-            elif event.key == pygame.K_UP: player.sprite.dy = -4
+            if event.key == pygame.K_LEFT : player.sprite.dangle = -2
+            elif event.key == pygame.K_RIGHT : player.sprite.dangle = 2
+            elif event.key == pygame.K_UP : move = True 
+            elif event.key == pygame.K_DOWN:
+                move = True
+                reverse = True
             elif event.key == pygame.K_SPACE and laser.sprite.shot == False: 
                 laser.sprite.shot = True
                 laser_sound.play()
-                laser.sprite.rect = laser.sprite.image.get_rect(center = (player.sprite.rect.midtop))
+                laser.sprite.rect = laser.sprite.image.get_rect(center = (player.sprite.rect.center))
+                laser.sprite.angle = player.sprite.angle
+                laser.sprite.dx = 15*math.sin(math.radians(laser.sprite.angle))
+                laser.sprite.dy = -15*math.cos(math.radians(laser.sprite.angle))
         if event.type == pygame.KEYUP:
-            if( event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT) : player.sprite.dx = 0
-            elif ( event.key == pygame.K_DOWN or event.key == pygame.K_UP) : player.sprite.dy = 0
+            if( event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT) : player.sprite.dangle = 0
+            elif ( event.key == pygame.K_UP): 
+                move = False
+                player.sprite.dangle = 0
+                player.sprite.dx = 0
+                player.sprite.dy = 0
+            elif ( event.key == pygame.K_DOWN): 
+                move = False
+                reverse = False
+                player.sprite.dangle = 0
+                player.sprite.dx = 0
+                player.sprite.dy = 0
         if event.type == enemy_timer:
-            enemy.add(Enemy())
+            newEnemy = Enemy()
+            enemy.add(newEnemy)
     screen.blit(background,(0,0))
     earth.draw(screen)
+    if(move):
+        player.sprite.dx = 4*math.sin(math.radians(player.sprite.angle))
+        player.sprite.dy = -4*math.cos(math.radians(player.sprite.angle))
+        if(reverse):
+            player.sprite.dx = -player.sprite.dx
+            player.sprite.dy = -player.sprite.dy
     player.update()
     player.draw(screen)
     if(laser.sprite.shot  == True):
